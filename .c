917b7e4db71c902d917ba4d5d55a70a45d41ec2b
Sajjad-arch <sajjadulislam870@gmail.com>
2024-12-07 22:26:15 +0600

    Add files via upload

---
+++ .c
@@ -0,0 +1,51 @@
+
+/*Write a program in C that calculates the number of values divisible by N that are less than or equal to
+M where N and M are integer numbers provided by user.*/
+
+
+
+
+ #include <stdio.h>
+
+  int main(void)
+  {
+      int M,N,O;
+      printf("Enter the Number of N ");
+
+      scanf("%d",N);
+
+      printf("Enter the Number of M ");
+      scanf("%d",M);
+      printf("Enter the Number of O ");
+      scanf("%d",O);
+
+      if (O%N==0&&O<=M)
+      {
+
+          printf("Yes");
+
+      }
+
+
+
+      else
+      {
+          printf("N0");
+      }
+
+
+
+
+      return 0;
+  }
+
+      printf("No");
+
+
+
+      Return 0,
+
+
+
+
+  }
